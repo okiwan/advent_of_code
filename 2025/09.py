3a0454d27d35aa40494fd893d250e47bb8516c88
Sergio Ocaña Gálvez <sergio.ocana@travelperk.com>
2026-01-06 16:12:25 +0200

    Day 9. Part 1 completed.

--- 2025/09.py
+++ 2025/09.py
@@ -27,8 +27,25 @@ def log(text):
 Part 1
 """
 
+def calculate_area(tile_1, tile_2):
+    x_diff = abs(tile_1[0] - tile_2[0]) + 1
+    y_diff = abs(tile_1[1] - tile_2[1]) + 1
+    return x_diff * y_diff
+
 def calculate_result():
-    return 0
+    """
+    The input is small, so let's try brute force
+    """
+
+    # Process the points
+    points = [(int(a[0]), int(a[1])) for a in [line.split(",") for line in data]]
+
+    max_area = 0
+    for i in range(0, len(points)):
+        for j in range(i + 1, len(points)):
+            max_area = max(max_area, calculate_area(points[i], points[j]))
+
+    return max_area
 
 result = calculate_result()
 print(f"> Part 1: {result}")
@@ -39,7 +56,15 @@ Part 2
 """
 
 def calculate_result_2():
+    """
+    Our approach is going to avoid performing the calculation for every
+    single combination, but rather select only the ones on the exterior
+    side (creating king of a bound box) and then perform the calculation
+    """
     return 0
 
 result = calculate_result_2()
 print(f"> Part 2: {result}")
+
+
+
